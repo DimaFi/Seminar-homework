7beb07d5d23f9b5dced78e600599c4d170201a55
DimaFi <53652727+DimaFi@users.noreply.github.com>
2022-11-17 22:58:10 +0400

    Update 17.11.2022_num_3.cpp

--- 17.11.2022_num_3.cpp
+++ 17.11.2022_num_3.cpp
@@ -1,16 +1,10 @@
-#define _USE_MATH_DEFINES
 #include <iostream>
-#include <string>
-#include <cctype>
 #include <algorithm>
 #include <vector>
 #include <cmath>
-#include <windows.h>
 #include <iomanip>
-#include <set>
 
 using namespace std;
-const long double g = 6.67430 * 1e-11;
 bool aa(double &a) {
   if (a==0) {
     	a = 0;
@@ -22,8 +16,6 @@ bool aa(double &a) {
 }
 
 int main() {
-	cin.tie(0);
-	ios_base::sync_with_stdio(0);
 	double a;
 	cin >> a;
 

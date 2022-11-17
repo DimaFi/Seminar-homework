1479548a30d7244e5eac05a5172a7cd6bd77b8f4
DimaFi <53652727+DimaFi@users.noreply.github.com>
2022-11-17 22:54:49 +0400

    Update 17.11.2022_num_4.cpp

--- 17.11.2022_num_4.cpp
+++ 17.11.2022_num_4.cpp
@@ -1,13 +1,7 @@
-#define _USE_MATH_DEFINES
 #include <iostream>
-#include <string>
-#include <cctype>
 #include <algorithm>
-#include <vector>
 #include <cmath>
-#include <windows.h>
 #include <iomanip>
-#include <set>
 
 using namespace std;
 
@@ -16,8 +10,6 @@ double aa(double &a, double &b) {
 }
 
 int main() {
-	cin.tie(0);
-	ios_base::sync_with_stdio(0);
 	double a, b;
 	cin >> a >> b;
 

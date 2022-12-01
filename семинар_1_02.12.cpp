0098c9a8ea488cef00cfb3b264f172247ab4be2d
DimaFi <53652727+DimaFi@users.noreply.github.com>
2022-12-01 22:07:03 +0400

    Add files via upload

---
+++ семинар_1_02.12.cpp
@@ -0,0 +1,27 @@
+﻿// семинар_1_02.12.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+double f(double n) {
+    if (n == 1) {
+        return 1;
+    }
+    if (n == 2) {
+        return 2;
+    }
+    return double(f(n - 1)) / f(n - 2) + 1;
+}
+
+int main()
+{
+    int i, n;
+    cin >> n;
+    for (i = 1; i <= n; i++) {
+        cout << i << ' ' << f(i) << endl;
+    }
+}
+

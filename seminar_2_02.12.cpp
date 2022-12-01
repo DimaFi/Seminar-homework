ef64a5b6c6ddcf1683368c6cb4ebd063e03c7c46
DimaFi <53652727+DimaFi@users.noreply.github.com>
2022-12-01 22:06:28 +0400

    Add files via upload

---
+++ seminar_2_02.12.cpp
@@ -0,0 +1,28 @@
+﻿// seminar_2_02.12.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+double f(double n, int x, int fac) {
+    return fac * pow(x, 2 * n + 1);
+}
+
+int main()
+{
+    int i, n, x, fac;
+    double s;
+    cin >> n >> x;
+    s = 0; 
+    fac = 1;
+    for (i = 1; i <= n; i++) {
+        fac *= i;
+        s += f(i, x, fac);
+    }
+    cout << s;
+}
+
+// 1    2    3  for x = 2
+// 8 + 64 + 768
